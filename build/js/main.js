89e4a4f72cce6412f32946ad5e037c54ca01d308
Kozyrev Ilya <kozyrev.frontend@gmail.com>
2020-05-05 17:28:17 +0300

    исправляет баг в навигации при переходе из моб меню

--- build/js/main.js
+++ build/js/main.js
@@ -22,3 +22,12 @@ navToggle.addEventListener('click', function () {
     logo.style.fill = '#f9fbfd';
   }
 });
+
+window.addEventListener('resize', function () {
+  if (screen.width >= 768) {
+    navMain.classList.add('page-header__nav--closed');
+    navMain.classList.remove('page-header__nav--opened');
+    navContainer.style.backgroundColor = 'transparent';
+    logo.style.fill = '#f9fbfd';
+  }
+});
